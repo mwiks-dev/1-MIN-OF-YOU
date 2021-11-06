5c87897110b0ef836607f32a7bee7d17fc6c10b2
Maryan23 <maryann.mwikali@student.moringaschool.com>
2021-11-06 16:51:40 +0300

    Creates and styles the top part of page

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
+bootstrap = Bootstrap()
 # Initializing application
 app = Flask(__name__)
 
--- app/views.py
+++ app/views.py
@@ -9,5 +9,4 @@ def index():
     View root page function that returns the index page and its data
     '''
     title = '1MinOfYou'
-    message = 'Welcome to 1 Min Of You'
-    return render_template('index.html',title = title,message = message)
+    return render_template('index.html',title = title)

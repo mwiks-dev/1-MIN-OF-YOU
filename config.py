5bb45dfed3033bdc52f6ac09e5ea9a7e1644a45b
Maryan23 <maryann.mwikali@student.moringaschool.com>
2021-11-09 16:57:50 +0300

    Changes before deployment

--- config.py
+++ config.py
@@ -1,17 +1,19 @@
+import os
 class Config:
     '''
     General configuration parent class
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:4543@localhost/minofyou'
     
 
     SECRET_KEY = 'Mwiks01'
+
+    DEBUG = True
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = "mwikali119@gmail.com"
-    MAIL_PASSWORD = "Kingzshiren12."
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
@@ -26,7 +28,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class DevConfig(Config):
@@ -39,7 +41,11 @@ class DevConfig(Config):
 
     DEBUG = True
 
+class TestConfig(Config):
+    
+    DEBUG = True
 config_options = {
 'development':DevConfig,
-'production':ProdConfig
+'production':ProdConfig,
+'test':TestConfig
 }
--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from app.models import User
 from flask_migrate import Migrate, MigrateCommand
 
 #Creating app instance
-app = create_app('development')
+app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server',Server)
@@ -15,6 +15,13 @@ manager.add_command('db',MigrateCommand)
 @manager.shell
 def make_shell_context():
     return dict(app = app,db=db,User = User)
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == '__main__':
     manager.run()
